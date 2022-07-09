1fe9ecb591946fe9f671e01a2c1278229653cdaf
Newon-universe <chokovon@gmail.com>
2022-07-09 23:54:45 +0900

    Feat : section 2

---
+++ Section 2. I am Rich/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Kim Yewon on 2022/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+ 
