ebde263cb2bba872d08ce92994d87eaebe03c3aa
Newon-universe <chokovon@gmail.com>
2022-07-12 18:24:36 +0900

    feat :  Section 6, 7

---
+++ Section 7. Xylophone/Xylophone-iOS13/Xylophone/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation // Audio Visual Foundational module
+
+class ViewController: UIViewController {
+
+    var player: AVAudioPlayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound("C")
+    }
+    
+    func playSound(_ input: String) {
+        guard let url = Bundle.main.url(forResource: input, withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+}
+
