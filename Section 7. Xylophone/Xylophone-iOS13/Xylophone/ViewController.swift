95a51db9d09ae0361ad1b429593272541bed0550
Newon-universe <chokovon@gmail.com>
2022-07-15 23:54:08 +0900

    feat : Section 7. Xylophone done

--- Section 7. Xylophone/Xylophone-iOS13/Xylophone/ViewController.swift
+++ Section 7. Xylophone/Xylophone-iOS13/Xylophone/ViewController.swift
@@ -11,32 +11,26 @@ import AVFoundation // Audio Visual Foundational module
 
 class ViewController: UIViewController {
 
-    var player: AVAudioPlayer?
+    var player: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound("C")
+        if let sound = sender.currentTitle {
+            sender.alpha = 0.5
+            playSound(sound)
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(200)) {
+                sender.layer.opacity = 1
+            }
+        }
     }
     
     func playSound(_ input: String) {
-        guard let url = Bundle.main.url(forResource: input, withExtension: "wav") else { return }
-
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-
-            guard let player = player else { return }
-
-            player.play()
-
-        } catch let error {
-            print(error.localizedDescription)
-        }
+        let url = Bundle.main.url(forResource: input, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
     }
     
 }
